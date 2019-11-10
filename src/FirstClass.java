9263101cd612e6d8489c07c94bc6d5304646e17f
samet.m <mohamadsamet@outlook.com>
2019-11-10 13:05:40 +0100

    lesson variable

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -3,7 +3,10 @@ public class FirstClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello");
+		int a = 955555559;
+		int b = a + 6;
+		float aa = (float)4.6;
+		System.out.println(b + aa);
 	}
 
 }
---
+++ src/lesson1/MyClass.java
@@ -0,0 +1,12 @@
+package lesson1;
+
+public class MyClass {
+/*
+ * My class main
+ */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("LOLO");
+	}
+
+}
