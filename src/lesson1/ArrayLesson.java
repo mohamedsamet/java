88138e750a0a048f6437144bb296f6ee3631ee19
samet.m <mohamadsamet@outlook.com>
2019-11-12 07:54:30 +0100

    For statement

--- src/lesson1/ArrayLesson.java
+++ src/lesson1/ArrayLesson.java
@@ -6,6 +6,12 @@ public class ArrayLesson {
 		// TODO Auto-generated method stub
 		int[] myArray = {1,3,2,5};
 		System.out.println(myArray[1]);
+		for (int index = 0; index < myArray.length; index++) {
+			System.out.println(myArray[index]);
+		}
+		for (int element : myArray) {
+			System.out.println(element);
+		}
 	}
 
 }
